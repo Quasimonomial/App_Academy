c0253ba18bf2063692deb168c3e497e19a087f60
App Academy Student <student@appacademy.io>
2014-10-21 11:52:55 -0700

    Create table definitions created

--- import_db.sql
+++ import_db.sql
@@ -7,13 +7,36 @@ CREATE TABLE users (
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title VARCHAR(255),
-  body VARCHAR(255),
+  body TEXT,
+  author_id INTEGER,
   FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_followers (
+  question_id INTEGER NOT NULL,
+  follower_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (follower_id) INTEGER REFERENCES users(id)/*DOES THIS NEED TO BE A FOREIGN KEY TO USERS? */
+);
+  
+
+CREATE TABLE replies (
+  reply_id INTEGER PRIMARY KEY, 
+  question_id INTEGER,
+  parent_id INTEGER, 
+  body TEXT,
+  author_id INTEGER,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(reply_id),
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes (
+  user_id INTEGER 
+  question_id INTEGER
+  FOREIGN KEY (user_id) REFERENCES users(id)
   FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
 
 
 
